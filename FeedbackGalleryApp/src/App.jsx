7618a4e519c7e5f4700b80f20bb9ec3d59cd08fa
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-02-18 23:22:06 +0600

    app component changes

--- FeedbackGalleryApp/src/App.jsx
+++ FeedbackGalleryApp/src/App.jsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
+import Animal from './components/category/animal/Animal'
+
+import Animals from './components/category/animal/Animals'
+import MainGallery from './components/MainGallery/MainGallery'
 
 function App() {
   return (
     <>
       <div>
         <Header></Header>
+        {/* <Animal /> */}
+        <MainGallery />
       </div>
 
     </>
